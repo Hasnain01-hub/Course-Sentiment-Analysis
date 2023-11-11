d0cfd09b9c3ae31108cbe969a27dff49ee88ac26
Hasnain Sayyed <70531302+Hasnain01-hub@users.noreply.github.com>
2023-11-11 17:03:41 +0530

    Update webScraping.py

--- webScraping.py
+++ webScraping.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 import urllib.request, json
 import requests
-
+import ssl
+import certifi
 # def WebScraping(link,site):
 #     return {
 #         'title' : 'Flask Course',
@@ -27,29 +28,31 @@ def WebScraping(link, platform):
         response = requests.get(url)
         htmlcontent = response.content
         soup = BeautifulSoup(htmlcontent, "html.parser")
-        c = soup.findAll('h1', {'class': 'banner-title banner-title-without--subtitle m-b-0'})
+        c = soup.findAll('h1', {'class': 'cds-119 cds-Typography-base css-1xy8ceb cds-121'})
         for i in c:
             result["title"] = i.text
         c = soup.findAll('div', {'class': 'content-inner'})
         for i in c:
             result["description"] = i.text
-        c = soup.findAll('div', {'class': '_16ni8zai m-b-0 m-t-1s'})
-        for i in c:
-            result["duration"] = i.text
-        c = soup.findAll('h3', {'class': 'instructor-name headline-3-text bold'})
-        for i in c:
-            result["instructor"] = i.text
-        c = soup.findAll('div', {'class': '_1fpiay2'})
-        for i in c:
-            result["learner_count"] = i.text
+        c = soup.findAll('div', {'class': 'cds-119 cds-Typography-base css-h1jogs cds-121'})
+        print(c)
+        result["duration"] = c[1].text
+        c = soup.findAll('span', {'class': 'cds-119 cds-Typography-base css-80vnnb cds-121'})
 
+        result["instructor"] = c[0].text
+        c = soup.findAll('div', {'class': 'cds-119 cds-Typography-base css-h1jogs cds-121'})
+        print(c)
+
+        result["learner_count"] = c[2].text
+        result["learner_no"]= c[0].text
         res=[]
 
-        url1 = url + "/reviews"
+        url1 = url + "#reviews"
         response = requests.get(url1)
         htmlcontent = response.content
         soup = BeautifulSoup(htmlcontent, "html.parser")
-        container = soup.findAll('div', {'class': 'css-18w79dz'})
+        container = soup.findAll('div', {'class': 'css-l6lvqi'})
+
         # res.append(container.text)
             # for j in container:
             #     comments.append(j.text)
@@ -66,9 +69,10 @@ def WebScraping(link, platform):
     elif site =="Youtube":
         result['v'] = link[link.index('=')+1:]
         videoId = c_url[(c_url.index("=") + 1):]
+        print(videoId)
         url = 'https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&id={}&key={}'.format(
-            videoId, 'AIzaSyDEgmEzXQ7GCRXqAa8ctgc6jA50vZJLhR4')
-        response = urllib.request.urlopen(url)
+            videoId, 'AIzaSyBmIvloIa6fEW-BwaSsDLJRwwH8lZvW5Os')
+        response = urllib.request.urlopen(url,context=ssl.create_default_context(cafile=certifi.where()))
         data = response.read()
         data = json.loads(data)
         duration = data["items"][0]["contentDetails"]["duration"]
@@ -81,15 +85,15 @@ def WebScraping(link, platform):
             result["duration"] = result["duration"] + ' ' +(duration[duration.find('T') + 1:duration.find('M')] + ' Mins')
 
         url = 'https://youtube.googleapis.com/youtube/v3/videos?part=statistics&id={}&key={}'.format(
-            videoId, 'AIzaSyDEgmEzXQ7GCRXqAa8ctgc6jA50vZJLhR4')
-        response = urllib.request.urlopen(url)
+            videoId, 'AIzaSyBmIvloIa6fEW-BwaSsDLJRwwH8lZvW5Os')
+        response = urllib.request.urlopen(url,context=ssl.create_default_context(cafile=certifi.where()))
         data = response.read()
         data = json.loads(data)
         result["learner_count"] = data["items"][0]["statistics"]["viewCount"] + ' views'
 
         url = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id={}&key={}'.format(
-            videoId, 'AIzaSyDEgmEzXQ7GCRXqAa8ctgc6jA50vZJLhR4')
-        response = urllib.request.urlopen(url)
+            videoId, 'AIzaSyBmIvloIa6fEW-BwaSsDLJRwwH8lZvW5Os')
+        response = urllib.request.urlopen(url,context=ssl.create_default_context(cafile=certifi.where()))
         data = response.read()
         data = json.loads(data)
         result["platform"] = "YouTube"
@@ -98,8 +102,8 @@ def WebScraping(link, platform):
         result["description"] = data["items"][0]["snippet"]["description"]
 
         url = 'https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=100&videoId={}&key={}'.format(
-            videoId, 'AIzaSyDEgmEzXQ7GCRXqAa8ctgc6jA50vZJLhR4')
-        response = urllib.request.urlopen(url)
+            videoId, 'AIzaSyBmIvloIa6fEW-BwaSsDLJRwwH8lZvW5Os')
+        response = urllib.request.urlopen(url,context=ssl.create_default_context(cafile=certifi.where()))
         data = response.read()
         data = json.loads(data)
         for i in range(len(data["items"])):
@@ -107,33 +111,4 @@ def WebScraping(link, platform):
                 comments.append(data['items'][i]['snippet']['topLevelComment']['snippet']['textDisplay'])
         result["comments"] = comments
         return result
-    
-    elif site == "Udemy":
-        videoId = c_url[51:(c_url.index('?')) - 1]
-        description_link="https://www.udemy.com/course/{}".format(videoId)
-        url = 'https://www.udemy.com/api-2.0/courses/{}/'.format(videoId)
-        response = urllib.request.urlopen(url)
-        data = response.read()
-        data = json.loads(data)
-        result["platform"] = "Udemy"
-        result["title"] = data["title"]
-        result["instructor"] = data["visible_instructors"][0]["display_name"]
-        response = requests.get(url)
-        htmlcontent = response.content
-        # soup = BeautifulSoup(htmlcontent, "html.parser")
-        # c = soup.findAll('div', {'data-purpose': 'safely-set-inner-html:description:description'})
-        result["description"] = 'N.A'
-        result["duration"] = 'N.A'
-        result["learner_count"] = 'N.A'
 
-
-        for i in range(1, 10):
-            url = 'https://www.udemy.com/api-2.0/courses/{}/reviews/?page={}'.format(videoId, i)
-            response = urllib.request.urlopen(url)
-            data = response.read()
-            data = json.loads(data)
-            for j in range(len(data["results"])):
-                if data["results"][j]["content"]!='':
-                    comments.append(data["results"][j]["content"])
-        result["comments"] = comments
-        return(result)
